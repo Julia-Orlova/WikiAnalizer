8f59c7b342f6cad2c1c3285ab6dbd45abdc3f02a
Julia-Orlova <66700142+Julia-Orlova@users.noreply.github.com>
2023-01-16 20:45:09 +0200

    Update Task 3.b.py

---
+++ pages/Task 3.b.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from main import task3a
+
+st.set_page_config(page_title="Task3b")
+
+st.markdown("# Task3.b. Retrieve a statistic of a particular user which include "
+            "topics to which the user has contributed the most.")
+
+if st.button('Rerun script'):
+    st.experimental_rerun()
+    
+user = st.text_input('Enter a username:')
+nums = int(st.number_input('Enter the number of days:', min_value=1, max_value=1000))
+
+if st.button('Retrieve a statistic'):
+    task3a(user, nums)
