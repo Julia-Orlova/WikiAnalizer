582da6c4a1e2e494237c0f4bdce61af96b9294cb
Julia-Orlova <66700142+Julia-Orlova@users.noreply.github.com>
2023-01-16 15:07:41 +0200

    Update Task 3.c.py

--- pages/Task 3.c.py
+++ pages/Task 3.c.py
@@ -9,9 +9,8 @@ st.markdown("# Task3.c. Retrieve a statistic of a particular user which include 
 if st.button('Rerun script'):
     st.experimental_rerun()
 
+user = st.text_input('Enter a username:')
+nums = int(st.number_input('Enter the number of days:', min_value=1, max_value=1000))
+
 if st.button('Retrieve a statistic'):
-    task3c()
-
-
-
-
+    task3c(user, nums)
